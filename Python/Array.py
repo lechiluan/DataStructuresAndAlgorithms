ddfd839b24b9619b95dcc171dbf8cd23954f721c
Lê Chí Luân <chiluan6601@gmail.com>
2022-08-04 14:40:36 +0700

    Create Array.py

---
+++ Python/Array.py
@@ -0,0 +1,27 @@
+# Array implementation in python
+def create_array():
+    array = []
+    return array
+
+def append(item):
+    self.array.append(item)
+    return self.array
+
+def pop(self):
+    return self.array.pop()
+
+def remove(item):
+    self.array.remove(item)
+    return self.array
+
+def clear(self):
+    self.array = []
+    return self.array
+
+def len(self):
+    return len(self.array)
+
+array = create_array()
+array.append(str(1))
+array.append(str(2))
+
